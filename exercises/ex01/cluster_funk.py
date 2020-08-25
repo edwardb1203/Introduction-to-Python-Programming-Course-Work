f2d1203f1e8dbd2e42706893e3a3ac82847e68ab
Edward Baker <eddean@email.unc.edu>
2020-08-25 16:40:06 -0400

    formatting

--- exercises/ex01/cluster_funk.py
+++ exercises/ex01/cluster_funk.py
@@ -1,12 +1,12 @@
 """Viral Calculator"""
-_author_ = "730312903"
+__author__ = "730312903"
 R0: str = input("Enter R0: ")
 t0: str = input("Enter t0 Cluster size: ")
 Viral_Growth_Rate = (float(R0))
 Cluster_Size = int(t0)
 Newly_Infected = round(int(Viral_Growth_Rate * Cluster_Size))
 Total_Infections = round(int(Newly_Infected + Cluster_Size))
-print ("t1 - New: " + str(Newly_Infected) + " - Total: " + str(Total_Infections))
-print ("t2 - New: " + str(round(Newly_Infected * Viral_Growth_Rate)) + " - Total: " + str(Total_Infections + round(Newly_Infected * Viral_Growth_Rate)))
-print ("t3 - New: " + str(round(Newly_Infected * Viral_Growth_Rate * Viral_Growth_Rate)) + " - Total: " + str(Total_Infections + round(Newly_Infected * Viral_Growth_Rate * Viral_Growth_Rate) + round(Newly_Infected * Viral_Growth_Rate)))
-print ("t4 - New: " + str(round(Newly_Infected * Viral_Growth_Rate * Viral_Growth_Rate * Viral_Growth_Rate)) + " - Total: " + str(Total_Infections + round(Newly_Infected * Viral_Growth_Rate * Viral_Growth_Rate * Viral_Growth_Rate) + round(Newly_Infected * Viral_Growth_Rate * Viral_Growth_Rate) + round(Newly_Infected * Viral_Growth_Rate)))
+print("t1 - New: " + str(Newly_Infected) + " - Total: " + str(Total_Infections))
+print("t2 - New: " + str(round(Newly_Infected * Viral_Growth_Rate)) + " - Total: " + str(Total_Infections + round(Newly_Infected * Viral_Growth_Rate)))
+print("t3 - New: " + str(round(Newly_Infected * Viral_Growth_Rate * Viral_Growth_Rate)) + " - Total: " + str(Total_Infections + round(Newly_Infected * Viral_Growth_Rate * Viral_Growth_Rate) + round(Newly_Infected * Viral_Growth_Rate)))
+print("t4 - New: " + str(round(Newly_Infected * Viral_Growth_Rate * Viral_Growth_Rate * Viral_Growth_Rate)) + " - Total: " + str(Total_Infections + round(Newly_Infected * Viral_Growth_Rate * Viral_Growth_Rate * Viral_Growth_Rate) + round(Newly_Infected * Viral_Growth_Rate * Viral_Growth_Rate) + round(Newly_Infected * Viral_Growth_Rate)))
