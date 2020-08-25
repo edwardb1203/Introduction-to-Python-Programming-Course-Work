f1e3acb711141aa0cd388008b7aaffa4b766b3b8
Edward Baker <eddean@email.unc.edu>
2020-08-25 14:29:11 -0400

    new program

---
+++ exercises/ex01/hype_machine.py
@@ -0,0 +1,6 @@
+"""HYPE PRODUCER"""
+_author_ = "730312903"
+name: str = input("What is your name? ")
+print( name + ", you are sexy and you know it! ") 
+print ( "Listen up, " + name + ", you are probably going to be the next Tony Stark...")
+print ("I got a call the other day, the Avengers are trying to recruit you " + name + "!")
