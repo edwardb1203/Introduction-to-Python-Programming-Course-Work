f19b4601afd5692c79101a242b5ed870e79742a2
Edward Baker <eddean@email.unc.edu>
2020-09-04 15:52:17 -0400

    fixed

--- exercises/ex03/cipher.py
+++ exercises/ex03/cipher.py
@@ -9,7 +9,6 @@ LETTER_SHIFT: int = 1
 
 def encode_char(letter: str) -> str:
     """Encodes lettter given +1 shift to the right in alphabet."""
-    letter: str = letter
     lower_letter: str = letter.lower()
     if letter == lower_letter:
         ascii_code: int = ord(letter)
@@ -18,11 +17,11 @@ def encode_char(letter: str) -> str:
         encoded_letter: str = chr(encoded_code)
         return encoded_letter
     else: 
-        ascii_code: int = ord(letter)
-        normalized_code: int = ascii_code - A_ASCII_NUMBER_UP
-        encoded_code: int = (normalized_code + LETTER_SHIFT) % LETTERS_ALPHABET + A_ASCII_NUMBER_UP
-        encoded_letter: str = chr(encoded_code)
-        return encoded_letter
+        ascii_code2: int = ord(letter)
+        normalized_code2: int = ascii_code2 - A_ASCII_NUMBER_UP
+        encoded_code2: int = (normalized_code2 + LETTER_SHIFT) % LETTERS_ALPHABET + A_ASCII_NUMBER_UP
+        encoded_letter2: str = chr(encoded_code2)
+        return encoded_letter2
 
 
 def encode_str(phrase: str) -> str:
@@ -30,8 +29,8 @@ def encode_str(phrase: str) -> str:
     i: int = 0
     encrypted_chr: str = ""
     while i < len(phrase):
-        character: str = phrase[i]
-        encrypted_chr += encode_char(character)
+        individ_ltr: str = phrase[i]
+        encrypted_chr += encode_char(individ_ltr)
         i = i + 1
     return encrypted_chr
 
@@ -40,17 +39,17 @@ def decode_char(character: str) -> str:
     """Decodes an encoded letter by shifting it back left -1."""
     character_lower: str = character.lower()
     if character == character_lower:
-        ascii_code: int = ord(character_lower)
-        normalized_code: int = ascii_code - A_ASCII_NUMBER
-        decoded_code: int = (normalized_code - LETTER_SHIFT) % LETTERS_ALPHABET + A_ASCII_NUMBER
+        ascii_code3: int = ord(character)
+        normalized_code3: int = ascii_code3 - A_ASCII_NUMBER
+        decoded_code: int = (normalized_code3 - LETTER_SHIFT) % LETTERS_ALPHABET + A_ASCII_NUMBER
         decoded_character: str = chr(decoded_code)
         return decoded_character
     else:
-        ascii_code: int = ord(character_lower)
-        normalized_code: int = ascii_code - A_ASCII_NUMBER_UP
-        decoded_code: int = (normalized_code - LETTER_SHIFT) % LETTERS_ALPHABET + A_ASCII_NUMBER_UP
-        decoded_character: str = chr(decoded_code)
-        return decoded_character
+        ascii_code4: int = ord(character)
+        normalized_code4: int = ascii_code4 - A_ASCII_NUMBER_UP
+        decoded_code2: int = (normalized_code4 - LETTER_SHIFT) % LETTERS_ALPHABET + A_ASCII_NUMBER_UP
+        decoded_character2: str = chr(decoded_code2)
+        return decoded_character2
 
 
 def decode_str(word: str) -> str:
@@ -58,11 +57,7 @@ def decode_str(word: str) -> str:
     i: int = 0
     decrypted_ltr: str = ""
     while i < len(word):
-        letter: str = word[i]
-        decrypted_ltr += decode_char(letter)
+        letter2: str = word[i]
+        decrypted_ltr += decode_char(letter2)
         i = i + 1
     return decrypted_ltr
-
-
-    if __name__ == "__encode_char__":
-        encode_char
